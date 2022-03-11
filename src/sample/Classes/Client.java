5e232bcab2daa49f320b8a29a53f02b489f8a341
= <=>
2022-03-11 16:48:09 +0100

    Original Commit, changes made to modify the strategy pattern

--- src/sample/Classes/Client.java
+++ src/sample/Classes/Client.java
@@ -1,12 +1,62 @@
 package sample.Classes;
 
-public class Client extends Personne{
+import java.util.Date;
 
+public class Client {
+    private int numCompte;
+    private String nomClient;
+    private String prenomClient;
+    private Double soldeClient;
+    private int agenceClient;
     private String dateRejointClient;
 
-    public Client(int numeroPersonne, String nomPersonne, String prenomPersonne, double salairePersonne, int agencePersonne, String dateRejointClient) {
-        super(numeroPersonne, nomPersonne, prenomPersonne, salairePersonne, agencePersonne);
+    public Client(int numCompte, String nomClient, String prenomClient, Double soldeClient, int agenceClient, String dateRejointClient) {
+        this.numCompte = numCompte;
+        this.nomClient = nomClient;
+        this.prenomClient = prenomClient;
+        this.soldeClient = soldeClient;
+        this.agenceClient = agenceClient;
         this.dateRejointClient = dateRejointClient;
+    }
+
+    public int getAgenceClient() {
+        return agenceClient;
+    }
+
+    public void setAgenceClient(int agenceClient) {
+        this.agenceClient = agenceClient;
+    }
+
+    public int getNumCompte() {
+        return numCompte;
+    }
+
+    public void setNumCompte(int numCompte) {
+        this.numCompte = numCompte;
+    }
+
+    public String getNomClient() {
+        return nomClient;
+    }
+
+    public void setNomClient(String nomClient) {
+        this.nomClient = nomClient;
+    }
+
+    public String getPrenomClient() {
+        return prenomClient;
+    }
+
+    public void setPrenomClient(String prenomClient) {
+        this.prenomClient = prenomClient;
+    }
+
+    public Double getSoldeClient() {
+        return soldeClient;
+    }
+
+    public void setSoldeClient(Double soldeClient) {
+        this.soldeClient = soldeClient;
     }
 
     public String getDateRejointClient() {
--- src/sample/Classes/Employe.java
+++ src/sample/Classes/Employe.java
@@ -1,12 +1,45 @@
 package sample.Classes;
 
-public class Employe extends Personne {
+public class Employe {
 
+    private int numEmploye;
+    private String nomEmploye;
+    private String prenomEmploye;
     private String cinEmploye;
+    private Double salaireEmploye;
+    private int agenceEmploye;
 
-    public Employe(int numeroPersonne, String nomPersonne, String prenomPersonne, double salairePersonne, int agencePersonne, String cinEmploye) {
-        super(numeroPersonne, nomPersonne, prenomPersonne, salairePersonne, agencePersonne);
+    public Employe(int numEmploye, String nomEmploye, String prenomEmploye, String cinEmploye, Double salaireEmploye, int agenceEmploye) {
+        this.numEmploye = numEmploye;
+        this.nomEmploye = nomEmploye;
+        this.prenomEmploye = prenomEmploye;
         this.cinEmploye = cinEmploye;
+        this.salaireEmploye = salaireEmploye;
+        this.agenceEmploye = agenceEmploye;
+    }
+
+    public int getNumEmploye() {
+        return numEmploye;
+    }
+
+    public void setNumEmploye(int numEmploye) {
+        this.numEmploye = numEmploye;
+    }
+
+    public String getNomEmploye() {
+        return nomEmploye;
+    }
+
+    public void setNomEmploye(String nomEmploye) {
+        this.nomEmploye = nomEmploye;
+    }
+
+    public String getPrenomEmploye() {
+        return prenomEmploye;
+    }
+
+    public void setPrenomEmploye(String prenomEmploye) {
+        this.prenomEmploye = prenomEmploye;
     }
 
     public String getCinEmploye() {
@@ -17,5 +50,19 @@ public class Employe extends Personne {
         this.cinEmploye = cinEmploye;
     }
 
+    public Double getSalaireEmploye() {
+        return salaireEmploye;
+    }
 
+    public void setSalaireEmploye(Double salaireEmploye) {
+        this.salaireEmploye = salaireEmploye;
+    }
+
+    public int getAgenceEmploye() {
+        return agenceEmploye;
+    }
+
+    public void setAgenceEmploye(int agenceEmploye) {
+        this.agenceEmploye = agenceEmploye;
+    }
 }
